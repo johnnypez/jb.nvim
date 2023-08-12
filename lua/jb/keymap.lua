3e97c3f1656bb771eb3c49e8f407aa3f11bf6c61
John Butler <john.butler@stitcherads.com>
2023-08-12 20:23:23 +0100

    pit stop

--- lua/jb/keymap.lua
+++ lua/jb/keymap.lua
@@ -6,8 +6,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Line Down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Line Up" })
 
 -- move between buffers
-vim.keymap.set("n", "]", "bn<CR>", { silent = false, desc = "Next Buffer" })
-vim.keymap.set("n", "[", "bp<CR>", { silent = false, desc = "Prev Buffer" })
+vim.keymap.set("n", "<leader>.", ":bn<CR>", { silent = true, desc = "Next Buffer" })
+vim.keymap.set("n", "<leader>,", ":bp<CR>", { silent = true, desc = "Prev Buffer" })
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
