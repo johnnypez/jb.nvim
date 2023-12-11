9e09461aace0a48d2df8b8bbfde0cbcabdde0226
John Butler <john.butler@kargo.com>
2023-12-11 20:53:10 +0000

    close buffers in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,29 @@
+local actions = require('telescope.actions')
+
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format("%s %s", "edit", j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 require('telescope').setup({
   defaults = {
     layout_strategy = 'vertical',
     layout_config = { height = 0.95 },
+    mappings = {
+      i = {
+        ["<CR>"] = select_one_or_multi,
+      },
+    },
   },
   extensions = {
     fzf = {
@@ -10,6 +32,15 @@ require('telescope').setup({
       override_file_sorter = true,    -- override the file sorter
       case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
     }
+  },
+  pickers = {
+    buffers = {
+      mappings = {
+        i = {
+          ["x"] = actions.delete_buffer,
+        },
+      },
+    },
   }
 })
 require('telescope').load_extension('fzf')
